866106e56f43594bc7932f9cb80a9007b4eeaa6e
Erik Rudisch <erudisch@meetyoo.de>
2019-06-20 22:21:39 +0200

    remove log and revoke_refresh_token right

--- authentication-server-admin-frontend/src/constants/index.js
+++ authentication-server-admin-frontend/src/constants/index.js
@@ -3,7 +3,7 @@ export const RESPONSE_TYPE = "code";
 export const CLIENT_ID = "auth_server";
 export const REDIRECT_URL = "http://localhost:8889/redirect.html";
 export const GRANT_TYPE = "authorization_code";
-export const SCOPE = "create_oauth read_oauth update_oauth delete_oauth revoke_refresh_token";
+export const SCOPE = "create_oauth read_oauth update_oauth delete_oauth";
 export const AUTHORIZATION_URL = `${OAUTH_SERVER}/oauth/authorize?response_type=${RESPONSE_TYPE}&scope=${SCOPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}`;
 
 export const SHOW_OAUTH_WINDOW = "SHOW_OAUTH_WINDOW";
