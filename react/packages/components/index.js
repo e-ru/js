0d0450ada4bbca7dbb3a2e3a69e8ff9ac43c7aa1
Erik Rudisch <erik.rudisch@posteo.de>
2019-07-25 21:58:32 +0200

    components package.json clean up

--- react/packages/components/index.js
+++
@@ -1,6 +0,0 @@
-export {
-  default as ButtonListItem
-} from "./src/components/common/List/ButtonListItem";
-export {
-  default as ExpandableButtonListItem
-} from "./src/components/common/List/ExpandableButtonListItem";
